13648844f8a41bc0be2ad47bb340d6d8ea2df8ae
karare1 <defi20@yahoo.co.uk>
2023-03-04 22:47:55 +0000

    Sign up and create a new user record in mongoDB

--- app.py
+++ app.py
@@ -27,6 +27,24 @@ def get_recipes():
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        exist_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if exist_user:
+            flash("A user with this username already exists")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "email": request.form.get("email"),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        session["new_user"] = request.form.get("username").lower()
+        flash("You have been registered!")
+
     return render_template("sign_up.html")
 
 
