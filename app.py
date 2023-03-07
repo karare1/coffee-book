049c5edf3b4e41858a4ee4b909e3fea9999f8d84
karare1 <defi20@yahoo.co.uk>
2023-03-07 00:06:12 +0000

    Change home.html to index.html

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 
 @app.route("/recipes")
