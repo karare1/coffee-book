fcd7e34c035bb1f84af9b171811528427c030df0
karare1 <defi20@yahoo.co.uk>
2023-03-08 10:06:17 +0000

    Add js code for adding and removing input fields in add_recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,73 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$(document).ready(function() {
+  $('input#recipe_name, input#recipe_intro').characterCounter();
+});
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+
+$(document).ready(function () {
+
+	$("#repeatDivBtn").click(function () {
+
+		$newid = $(this).data("increment");
+		$repeatDiv = $("#repeatDiv").wrap('<div/>').parent().html();
+		$('#repeatDiv').unwrap();
+		$($repeatDiv).insertAfter($(".repeatDiv").last());
+		$(".repeatDiv").last().attr('id',   "repeatDiv" + '_' + $newid);
+		$("#repeatDiv" + '_' + $newid).append('<div class="input-field"><button type="button" class="waves-effect waves-light btn removeDivBtn" data-id="repeatDiv'+'_'+ $newid+'">Remove</button></div>');
+		$newid++;
+		$(this).data("increment", $newid);
+
+	});
+
+
+	$(document).on('click', '.removeDivBtn', function () {
+
+		$divId = $(this).data("id");
+		$("#"+$divId).remove();
+		$inc = $("#repeatDivBtn").data("increment");
+		$("#repeatDivBtn").data("increment", $inc-1);
+
+	});
+
+});
+
+
+$(document).ready(function () {
+
+	$("#repeatBtn").click(function () {
+
+		$newid = $(this).data("increment");
+		$repeatDiv = $("#repDiv").wrap('<div/>').parent().html();
+		$('#repDiv').unwrap();
+		$($repeatDiv).insertAfter($(".repDiv").last());
+		$(".repDiv").last().attr('id',   "repDiv" + '_' + $newid);
+		$("#repDiv" + '_' + $newid).append('<div class="input-field"><button type="button" class="waves-effect waves-light btn removeBtn" data-id="repDiv'+'_'+ $newid+'">Remove</button></div>');
+		$newid++;
+		$(this).data("increment", $newid);
+
+	});
+
+
+	$(document).on('click', '.removeBtn', function () {
+
+		$divId = $(this).data("id");
+		$("#"+$divId).remove();
+		$inc = $("#repeatBtn").data("increment");
+		$("#repeatBtn").data("increment", $inc-1);
+
+	});
+
+});
+
+
+
+
+
+
